ea7ca734d40ae3880cb161cad38c8af082ac83c2
andrei andrei <andrei@joyspay.com>
2025-04-28 08:22:01 +0300

    feat: solve the subtasks streams

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,30 @@
+import { Transform } from 'stream';
+import { stdin, stdout } from 'process';
+
 const transform = async () => {
-    // Write your code here 
+    const reverseStream = new Transform({
+        transform(chunk, encoding, callback) {
+            const reversed = chunk.toString().split('').reverse().join('');
+            callback(null, reversed + '\n\n');
+        }
+    });
+
+    stdin.on('error', () => {
+        throw new Error('Input stream error');
+    });
+
+
+    reverseStream.on('error', () => {
+        throw new Error('Transform stream error');
+    });
+
+    stdout.on('error', () => {
+        throw new Error('Output stream error');
+    });
+
+    console.log('Input the text and press ENTER')
+
+    stdin.pipe(reverseStream).pipe(stdout);
 };
 
 await transform();
--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,22 @@
+import { createWriteStream } from 'fs';
+import { join } from 'path';
+
 const write = async () => {
-    // Write your code here 
+    const path = join('src/streams/files', 'fileToWrite.txt');
+    const writableStream = createWriteStream(path);
+
+    writableStream.on('error', (err) => {
+        console.error('FS operation failed:', err.message);
+        process.exit(1);
+    });
+
+    console.log('Please enter the text. To finish, press Ctrl+Z (for Windows) or Ctrl+D (for Mac).')
+
+    process.stdin.pipe(writableStream);
+
+    writableStream.on('finish', () => {
+        console.log('\nThe recording was completed successfully. Check the contents of the file fileToWrite.txt');
+    });
 };
 
 await write();
