a23037966c5401f8832b5ec81615b391a791d1a4
andrei andrei <andrei@joyspay.com>
2025-05-04 22:20:44 +0300

    feat:add command processing

--- src/index.js
+++ src/index.js
@@ -17,3 +17,72 @@ const rl = readline.createInterface({
     output: process.stdout,
     prompt: '> ',
 });
+
+rl.on('SIGINT', () => {
+    exitProgram();
+});
+
+rl.prompt();
+
+rl.on('line', async (line) => {
+    const input = line.trim();
+
+    if (input === '.exit') {
+        exitProgram();
+        return;
+    }
+
+    try {
+        await handleCommand(input);
+    } catch {
+        console.log('Operation failed');
+    }
+
+    printCurrentDirectory();
+    rl.prompt();
+});
+
+async function handleCommand(input) {
+    const [command, ...args] = input.split(' ');
+
+    switch (command) {
+        case 'up':
+            moveUp();
+            break;
+
+        case 'cd':
+            if (!args[0]) {
+                console.log('Invalid input');
+                return;
+            }
+            await changeDirectory(args[0]);
+            break;
+
+        default:
+            console.log('Invalid input');
+    }
+}
+
+async function changeDirectory(dir) {
+    const targetPath = path.resolve(currentDir, dir);
+    try {
+        const stats = await fs.stat(targetPath);
+        if (!stats.isDirectory()) throw new Error();
+
+        const root = path.parse(currentDir).root;
+        if (!targetPath.startsWith(root)) throw new Error();
+
+        currentDir = targetPath;
+    } catch {
+        throw new Error('Operation failed');
+    }
+}
+
+function moveUp() {
+    const parentDir = path.dirname(currentDir);
+    const root = path.parse(currentDir).root;
+
+    if (currentDir !== root) {
+        currentDir = parentDir;
+    }
+}
